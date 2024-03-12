87c346f00410e967f3ff3df69a374f79bd7314f3
ranjan <ranjan.mj@plintron.com>
2024-03-12 13:56:57 +0530

    new console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Page() {
-  console.log("browser");
+  console.log("browsers");
   return (
     <main className="p-4 flex flex-col h-full items-center">
       <Button asChild>
